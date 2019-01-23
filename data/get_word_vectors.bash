6c964eb0eb1c6014bf3dff1f5720380c823cef78
KeksimusPrime <46974145+KeksimusPrime@users.noreply.github.com>
2019-01-23 20:07:23 +0000

    created data download files

---
+++ data/get_word_vectors.bash
@@ -0,0 +1,25 @@
+glovepath='http://nlp.stanford.edu/data/glove.840B.300d.zip'
+fasttextpath='https://s3-us-west-1.amazonaws.com/fasttext-vectors/crawl-300d-2M.vec.zip'
+GoogleNewspath='https://github.com/mmihaltz/word2vec-GoogleNews-vectors/blob/master/GoogleNews-vectors-negative300.bin.gz'
+SIF='https://github.com/PrincetonML/SIF/raw/master/auxiliary_data/enwiki_vocab_min200.txt'
+
+
+mkdir word_vectors
+mkdir auxiliary_data
+cd word_vectors
+
+curl -LO $glovepath
+unzip glove.840B.300d.zip 
+rm glove.840B.300d.zip
+
+curl -LO $fasttextpath
+unzip crawl-300d-2M.vec.zip 
+rm crawl-300d-2M.vec.zip
+
+curl -LO $GoogleNewspath
+gunzip GoogleNews-vectors-negative300.bin.gz
+python -c "from gensim import word2vec; model = word2vec.Word2Vec.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)"
+
+cd ../auxiliary_data
+curl -LO $SIF
+cd ..
